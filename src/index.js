a834af3222b33ed0146a9864456c3110f0ee22aa
J Sai Charan Reddy <jsaicharanreddy0605@gmail.com>
2024-02-01 21:59:50 +0530

    simple changes

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>React Pizza.co</h1>;
+}
+function Menu() {
+  return (
+    <div>
+      <h1>this is menu</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+function Footer() {
+  const hour = new Date().getHours();
+  const open = 10;
+  const close = 22;
+  const isOpen = (hour) => hour >= open && hour <= close;
+  console.log(isOpen);
+  return (
+    <p>
+      time is
+      {hour} hours so: This Store is now Open
+    </p>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+      <h1>Pizza Spinaci</h1>
+      <p> Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
